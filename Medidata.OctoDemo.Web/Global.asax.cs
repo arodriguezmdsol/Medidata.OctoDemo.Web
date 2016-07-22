c740e3d57788f76fc666c468d03906ce092f334f
arodriguez <arodriguez@mdsol.com>
2016-07-22 00:46:00 -0400

    initial

---
+++ Medidata.OctoDemo.Web/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Medidata.OctoDemo.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "{controller}"
+                );
+            });
+        }
+        
+    }
+}
---
+++ Medidata.OctoDemo.Web/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Medidata.OctoDemo.Web
+{
+    [Route("")]
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { message = ConfigurationManager.AppSettings["Message"] });
+        }
+        
+    }
+}
